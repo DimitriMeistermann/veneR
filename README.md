6e5ee6526b1188e02fdf51a96b5a88888351a3f4
DimitriMeistermann <dimitri.meistermann@univ-nantes.fr>
2021-03-06 23:16:49 +0100

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 Required functions for my scripts
 ==========================================
 
-See *loadFun.R* to see how to load the functions.
-
 # Load functions
 ```
-source("https://gitlab.univ-nantes.fr/E114424Z/veneR/-/raw/master/loadFun.R")
+source("https://raw.githubusercontent.com/DimitriMeistermann/veneR/main/loadFun.R")
 ```
---
+++ loadFun.R
@@ -0,0 +1,6 @@
+#Import all functions
+source("https://raw.githubusercontent.com/DimitriMeistermann/veneR/main/RNASeq.R?inline=false")
+source("https://raw.githubusercontent.com/DimitriMeistermann/veneR/main/general.R?inline=false")
+source("https://raw.githubusercontent.com/DimitriMeistermann/veneR/main/projection.R?inline=false")
+source("https://raw.githubusercontent.com/DimitriMeistermann/veneR/main/deprecated.R?inline=false")
+
